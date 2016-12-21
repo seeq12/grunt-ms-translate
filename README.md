786730f980b1d7c5b410d4c6b3640d5e85ec8c4c
Jason Rust <rustyparts@gmail.com>
2016-12-21 15:42:34 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,97 @@
+# grunt-ms-translate
+
+> A build task to translate JSON files to other languages using Microsoft's Translation API. Pairs very well with angular-translate.
+
+Massive thanks to [dolanmiu](https://github.com/dolanmiu) for the
+[grunt-google-translate](https://github.com/dolanmiu/grunt-google-translate)
+that this is heavily based off of. It was created to leverage Microsoft's
+[Translator API](https://www.microsoft.com/en-us/translator/getstarted.aspx)
+which has the benefit of having a free tier.
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-ms-translate --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-ms-translate');
+```
+
+## The "ms_translate" task
+
+### Overview of Options
+
+#### options.msApiKey
+Type: `String`
+Default value: `',  '`
+
+The API key used to access [Microsoft's Translation API](https://www.microsoft.com/en-us/translator/getstarted.aspx).
+
+### Usage Examples
+
+#### Simple Example
+In this example, I passed in a JSON file with english text. It will then create two files in the ```i18n/``` folder called ru.json and zh-CN.json for Russian and Chinese respectively.
+
+Note: This plugin will try and deduce the suffix (file type), so you don't need to explicity specify it. If you need it to have a different suffix, then specify the ```suffix``` as shown in the next example.
+
+```js
+grunt.initConfig({
+    ms_translate: {
+        default_options: {
+            options: {
+                msApiKey: YOUR_API_KEY_HERE
+            },
+            files: [{
+                src: '<%= yeoman.client %>/i18n/en.json',
+                sourceLanguage: 'en',
+                targetLanguages: ['ru', 'zh-CN'],
+                dest: '<%= yeoman.client %>/i18n/'
+            }]
+        }
+    }
+});
+```
+
+#### Full Example
+In this example, two files are being translated, one called ```locale-en.json``` and another called ```locale-fr.json```. They are in different folders, and will create translated files in the same ```i18n/``` folder.
+
+Notice how the prefix and suffix is specified, it means the translated files will be named like ```locale-de.json``` instead of ```de.json```.
+
+```js
+grunt.initConfig({
+    ms_translate: {
+        default_options: {
+            options: {
+                msApiKey: YOUR_API_KEY_HERE
+            },
+            files: [{
+                src: '<%= yeoman.client %>/i18n/locale-en.json',
+                sourceLanguage: 'en',
+                targetLanguages: ['ru', 'zh-CN'],
+                dest: '<%= yeoman.client %>/i18n/',
+                prefix: 'locale-',
+                suffix: '.json'
+            }, {
+                src: '<%= yeoman.client %>/specialFolder/locale-fr.json',
+                sourceLanguage: 'fr',
+                targetLanguages: ['de', 'zh-CN'],
+                dest: '<%= yeoman.client %>/i18n/',
+                prefix: 'locale-',
+                suffix: '.json'
+            },]
+        }
+    }
+});
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
---
+++ tasks/ms_translate.js
@@ -0,0 +1,142 @@
+/*
+ * grunt-ms-translate
+ * Translates strings using the microsoft translation API
+ *
+ * Copyright (c) 2016 Jason Rust
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+const _ = require('lodash');
+const Promise = require('bluebird');
+const variableDictionary = {};
+// See https://msdn.microsoft.com/en-us/library/ff512403.aspx
+const MAX_TRANSLATE_ELEMENTS = 10;
+
+function deepTraverseJson(json, cb) {
+  _.forOwn(json, function(value, key) {
+    if (_.isObject(value)) {
+      deepTraverseJson(value, cb);
+      return;
+    }
+
+    cb(json, value, key);
+  });
+}
+
+function findVariables(str, regex) {
+  const match = str.match(regex);
+  return _.isNull(match) ? [] : match;
+}
+
+function addToVariableDictionary(originalString) {
+  // Matches nested variables, up to 2 levels deep, e.g. "Error: {hasError, select, true{: {message}} other{}}."
+  // See http://stackoverflow.com/a/35271017/1108708
+  const variableRegex = /({(?:[^}{]+|{(?:[^}{]+|{[^}{]*})*})*})/ig;
+  const variables = findVariables(originalString, variableRegex);
+  let currentString = originalString;
+
+  variables.forEach(function(variable) {
+    const destinationVar = 'GRUNT_MS_TRANSLATE_VAR_' + Object.keys(variableDictionary).length;
+    variableDictionary[destinationVar] = variable;
+    currentString = currentString.replace(variable, destinationVar);
+  });
+
+  return currentString;
+}
+
+function createVariableSafeJson(origJson) {
+  const sourceJson = _.cloneDeep(origJson);
+
+  deepTraverseJson(sourceJson, function(parent, value, key) {
+    const varSafeString = addToVariableDictionary(value);
+    parent[key] = varSafeString;
+  });
+
+  return sourceJson;
+}
+
+function revertVariablesInJson(translatedJson) {
+  const replacedRegex = /(GRUNT_MS_TRANSLATE_VAR_\d+)/g;
+  const sourceJson = _.cloneDeep(translatedJson);
+
+  deepTraverseJson(sourceJson, function(parent, value, key) {
+    const variables = findVariables(value, replacedRegex);
+
+    variables.forEach(function(variable) {
+      parent[key] = parent[key].replace(variable, variableDictionary[variable]);
+    });
+  });
+
+  return sourceJson;
+}
+
+function translate(origJson, translateArray, source, target, destPath, grunt) {
+  const jsonReferenceArray = [];
+  const sourceJson = _.cloneDeep(origJson);
+
+  deepTraverseJson(sourceJson, function(parent, value, key) {
+    jsonReferenceArray.push({
+      parent: parent,
+      value: value,
+      key: key
+    });
+  });
+
+  grunt.log.writeln('Translating into: ' + target);
+  return _.chain(jsonReferenceArray)
+    .map('value')
+    .chunk(MAX_TRANSLATE_ELEMENTS)
+    .map((translateChunk) => translateArray({ texts: translateChunk, from: source, to: target }))
+    .thru(Promise.all)
+    .value()
+    .then((translatedChunks) => {
+      const translations = _.flatten(translatedChunks);
+      _.forEach(jsonReferenceArray, (jsonReference, i) => {
+        // parent is a reference to the JSON, so changing it updates sourceJson
+        jsonReferenceArray[i].parent[jsonReferenceArray[i].key] = translations[i].TranslatedText;
+      });
+
+      return { dest: destPath, json: sourceJson };
+    });
+}
+
+module.exports = function(grunt) {
+  grunt.registerMultiTask('ms_translate',
+    'A build task to translate JSON files to other languages using Microsoft\'s Translation API. Pairs very well with angular-translate.',
+    function() {
+      const done = this.async();
+      const MsTranslator = require('mstranslator');
+      const msTranslate = new MsTranslator({ api_key: this.options().msApiKey }, true);
+      const translateArray = Promise.promisify(msTranslate.translateArray, { context: msTranslate });
+
+      _.chain(this.files)
+        .map(function(file) {
+          file.prefix = file.prefix || '';
+          file.suffix = file.suffix || /.+(\..+)/.exec(file.src)[1];
+
+          const languageJson = JSON.parse(grunt.file.read(file.src));
+          const variableSafeJson = createVariableSafeJson(languageJson);
+
+          return _.map(file.targetLanguages, function(targetLanguage) {
+            const filePath = file.dest + file.prefix + targetLanguage + file.suffix;
+            return translate(variableSafeJson, translateArray, file.sourceLanguage, targetLanguage, filePath, grunt);
+          });
+        })
+        .flatten()
+        .thru(Promise.all)
+        .value()
+        .then((translatedJsons) => {
+          grunt.log.writeln('Writing translated file');
+          _.forEach(translatedJsons, function(translatedJson) {
+            const revertedJson = revertVariablesInJson(translatedJson.json);
+            grunt.file.write(translatedJson.dest, JSON.stringify(revertedJson, null, '  '));
+            grunt.log.writeln('Wrote translated file: ' + translatedJson.dest);
+          });
+
+          done();
+        })
+        .catch(grunt.fail.fatal);
+    });
+};
